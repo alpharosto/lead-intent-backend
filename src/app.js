ecf36829ca1b06c0cea4ea234bd17f3889b82a65
alpharosto <kharatsaurav2023@gmail.com>
2025-11-01 01:12:46 +0530

    cleanup

--- src/app.js
+++ src/app.js
@@ -4,17 +4,18 @@ import offerRoutes from './routes/offer.routes.js'
 import leadsRoutes from './routes/leads.routes.js';
 import scoreRoutes from './routes/score.routes.js';
 import resultsRoutes from './routes/results.routes.js';
-import { errorHandler } from './middlewares/errorHandler.js';
+
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use(errorHandler);
 
 app.use('/offer', offerRoutes);
 app.use('/leads', leadsRoutes);
-app.use('/', scoreRoutes);
-app.use('/', resultsRoutes);
-app.get('/', (_, res) =>res.send('Lead Intent Scoring API'));
+app.use('/score', scoreRoutes);
+app.use('/result', resultsRoutes);
+
 app.get('/health', (_, res) => res.json({ ok: true }));
+
+
 
 export default app;
